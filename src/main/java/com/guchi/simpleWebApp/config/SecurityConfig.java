14610db09647b3c258775859486355d0ade27467
Chitransh Agrawal <chitranshaga.2345@gmail.com>
2026-02-02 14:15:09 +0530

    csrf token disable

--- src/main/java/com/guchi/simpleWebApp/config/SecurityConfig.java
+++ src/main/java/com/guchi/simpleWebApp/config/SecurityConfig.java
@@ -1,24 +1,38 @@
-//package com.guchi.simpleWebApp.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//
-//@Configuration
-//public class SecurityConfig {
-//
-//    @Bean
-//    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf(csrf -> csrf.disable())
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/h2-console/**").permitAll()
-//                        .anyRequest().permitAll()
-//                )
-//                .headers(headers -> headers.frameOptions(frame -> frame.disable()));
-//
-//        return http.build();
-//    }
-//}
-//
+package com.guchi.simpleWebApp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity // i don't want default security configuration, i want it here
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+//        http.csrf(customizer -> customizer.disable()); // disable csrf
+//        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+////        http.formLogin(Customizer.withDefaults()); // browser
+//        http.httpBasic(Customizer.withDefaults()); // rest api
+//        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+
+        // builder pattern
+        return http
+                .csrf(customizer -> customizer.disable()) // disable csrf
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults()) // rest api
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+
+//        http.formLogin(Customizer.withDefaults()); // browser
+
+//        return http.build(); // build returns the object of security filter chain
+    }
+}
