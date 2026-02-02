75e2f40de640ca936ea050373549d69cb5e9359b
Chitransh Agrawal <chitranshaga.2345@gmail.com>
2026-02-02 12:26:20 +0530

    spring security dependency added

--- src/main/java/com/guchi/simpleWebApp/model/Product.java
+++ src/main/java/com/guchi/simpleWebApp/model/Product.java
@@ -2,26 +2,40 @@ package com.guchi.simpleWebApp.model;
 
 //import jakarta.persistence.Entity;
 //import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+//import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+//import org.springframework.data.mongodb.core.mapping.Document;
+
+
+//@Data
+//@AllArgsConstructor
+////@Entity
+//@NoArgsConstructor
+////@Document // Mongodb document (it tells that it is the database)
+//public class Product {
+//
+//    @Id // primary key
+//    private int productId;
+//    private String productName;
+//    private int price;
+//
+//}
 
 
 @Data
 @AllArgsConstructor
-//@Entity
 @NoArgsConstructor
-@Document // Mongodb document (it tells that it is the database)
+@Entity
+//@Table(name="table_1") // (we can specify table name here or else it will take the class name as table name)
 public class Product {
 
-    @Id // primary key
+    @Id
     private int productId;
     private String productName;
     private int price;
-
 }
-
-
 
--- src/main/java/com/guchi/simpleWebApp/repository/ProductRepo.java
+++ src/main/java/com/guchi/simpleWebApp/repository/ProductRepo.java
@@ -2,7 +2,8 @@ package com.guchi.simpleWebApp.repository;
 
 import com.guchi.simpleWebApp.model.Product;
 //import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.mongodb.repository.MongoRepository;
+//import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
@@ -11,6 +12,9 @@ import org.springframework.stereotype.Repository;
 //
 //}
 
+//@Repository
+//public interface ProductRepo extends MongoRepository<Product, Integer> // <className (in model), data type of primary key>
+//{}
+
 @Repository
-public interface ProductRepo extends MongoRepository<Product, Integer> // <className (in model), data type of primary key>
-{}
+public interface ProductRepo extends JpaRepository<Product, Integer> {}
